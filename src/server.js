6ed73d9489a91d3b90b2e2b008c0c69fce8732b3
Murillo Yamamoto <mu.yavi@gmail.com>
2023-12-03 18:11:16 -0300

    .

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+process.env.NODE_OPTIONS = '--openssl-legacy-provider';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
